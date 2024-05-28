d0112a0950109928f94e39257d736570a64c2c12
Mónica Salvatierra <sal22249@uvg.edu.gt>
2024-05-27 23:16:30 -0600

    final changes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import Display from "@/components/Display";
 import Keypad from "@/components/Keypad";
 import styles from '@/styles/calculator.module.scss';
